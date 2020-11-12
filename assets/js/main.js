3a431f4e94d6c604b7bf5c6f05b1f22ad76e80b8
antonio <antoniorocca46@gmail.com>
2020-11-12 17:39:20 +0100

    Final Commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var userName = prompt("Come ti chiami?");
+var userSurname = prompt("Qual è il tuo cognome?");
+var userColor = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById('write').innerHTML =
+userName + userSurname + userColor + 19;
